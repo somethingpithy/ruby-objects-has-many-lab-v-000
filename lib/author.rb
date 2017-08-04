ce6219ca743f5bf23011da8fcdc3b40d68fd0579
Carlota Bindner <cev.bindner@gmail.com>
2017-08-04 02:32:12 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,21 @@
+class Author
+  attr_accessor :name
+
+  @@post_count = 0
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    self.posts << post
+    post.artist = self
+    @@post_count += 1
+  end
+
+end
