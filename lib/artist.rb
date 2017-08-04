e1815aa754e79e6f5fd271a29e95e9058bf84d93
Carlota Bindner <cev.bindner@gmail.com>
2017-08-04 02:26:01 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist
-  attr_accessor :name, :artist
+  attr_accessor :name
 
   @@song_count = 0
 
@@ -9,14 +9,14 @@ class Artist
   end
 
   def add_song(song)
-    @songs << song
+    self.songs << song
     song.artist = self
     @@song_count += 1
   end
 
   def add_song_by_name(song)
     song = Song.new(name)
-    @songs << song
+    self.songs << song
     song.artist = self
     @@song_count += 1
   end
