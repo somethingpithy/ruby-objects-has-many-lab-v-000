7f0f0b4d0378710ba4a52ed853c82c9da40246a4
Carlota Bindner <cev.bindner@gmail.com>
2017-08-04 02:16:57 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,4 +11,14 @@ class Artist
     song.artist = self
   end
 
+  def add_song_by_name(song)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+  end
+    
+
+  def self.song_count
+  end
+
 end
